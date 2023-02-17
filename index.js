2d9f4f7e6e38f7e1eeb7b9602a6a4d6789399adf
Shubham Tripathi <shubhhtr@gmail.com>
2023-02-17 21:50:05 +0530

    added database setup

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const express=require("express");
+const chats=require("./data/data");
 const app=express();
 
-app.get("/test", (req,resp)=>{
-    resp.send("working");
+app.use(express.json());
+
+app.get("/", (req,resp)=>{
+    resp.send("api is working");
+})
+
+app.get("/chat", (req,resp)=>{
+    resp.send(chats);
+})
+
+app.get("/chat/:id", (req,resp)=>{
+    const data=chats.find((e)=>e._id===req.params.id);
+    resp.send(data);
 })
 
 app.listen(8080);
